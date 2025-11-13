f57ed850216bfa606b4c89232832fccf7a37da8f
David Paul Graham <dpgraham4401@gmail.com>
2025-11-13 08:29:51 -0500

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ]
+});
